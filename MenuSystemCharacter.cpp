1eda928827d9f02f210ca81874b89c1dc6919e06
hyadmin123 <105160460+hyadmin123@users.noreply.github.com>
2024-11-03 13:12:13 +0800

    Add files via upload

---
+++ MenuSystemCharacter.cpp
@@ -0,0 +1,27 @@
+﻿#include "MenuSystemCharacter.h"
+
+#include "Interfaces/OnlineSessionInterface.h"
+
+MenuSystemCharacter::MenuSystemCharacter()
+{
+	IOnlineSubsystem * OnlineSubsystem = IOnlineSubsystem::Get();
+
+	if (OnlineSubsystem)
+	{
+		OnlineSessionInterface = OnlineSubsystem->GetSessionInterface();
+	}
+
+	OnlineSessionInterface =  OnlineSubsystem->GetSessionInterface();
+	//在UE中打印出来
+	if (GEngine) //游戏运行时指向当前游戏世界的实例
+	{
+		    GEngine->AddOnScreenDebugMessage(
+			-1,
+			-15.f,
+			FColor::Blue,
+			FString::Printf(TEXT("Found subsystem %s"),*OnlineSubsystem->GetSubsystemName().ToString())
+			);
+	}
+
+	
+}
---
+++ MenuSystemCharacter.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineSubsystem.h"
+class MenuSystemCharacter
+{
+public:
+	
+
+
+	
+	 MenuSystemCharacter();
+	~MenuSystemCharacter();
+
+
+	//只需用T共享指针将指针指向I在线会话
+	TSharedPtr<class IOnlineSession,ESPMode::ThreadSafe> OnlineSessionInterface;
+
+public:
+	
+};
